4b430cd71548714106eba738f2ea5726f04e9c30
devika-nair1 <87757745+devika-nair1@users.noreply.github.com>
2021-07-26 10:42:15 +0530

    Add files via upload

---
+++ mySecondReact/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+var element = React.createElement('h1', { className: 'greeting' }, 'Hello, world!');
+ReactDOM.render(element, document.getElementById('root'));
+
+
+export default App;
+
